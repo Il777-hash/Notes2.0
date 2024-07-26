813f22904d895336acec1bab1c7d2f26d3942606
Ilfat <ilfat95@mail.ru>
2024-07-26 17:00:08 +0500

    Interfaces

---
+++ Notes.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Notes.Application.Interfaces
+{
+    public interface IRepository<T>
+    {
+
+        public Task<Guid> Create(T item);
+        public Task<Unit> Delete(Guid id);
+        public Task<Guid> Update(T item);
+        public Task<T> Get(Guid id);
+        public Task<IEnumerable<T>> GetAll();
+    }
+}
---
+++ Notes.Persistence/Interfaces/INotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain.Models;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        public DbSet<Reminder> Reminders { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
