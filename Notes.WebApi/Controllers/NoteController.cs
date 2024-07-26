95b41ea9299afa1b7f61e913ec8eee2aca667710
Ilfat <ilfat95@mail.ru>
2024-07-26 17:07:33 +0500

    Controllers

---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Commands.Create;
+using Notes.Controllers;
+
+namespace Notes.WebApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("[controller]")]
+
+    public class NoteController : ControllerMain
+    {
+        [HttpPost("create")]
+        public async Task<ActionResult<Guid>> Create([FromBody] CreateNoteCommand command)
+        {
+            var id = await Mediator.Send(command);
+            return Ok(id);
+        }
+    }
+}
