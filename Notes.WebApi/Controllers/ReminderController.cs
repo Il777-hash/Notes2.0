fed775c6aabc0de8cc3286f214970f3739696e20
Ilfat <ilfat95@mail.ru>
2024-07-28 18:42:29 +0500

    note, reminder controllers

---
+++ Notes.WebApi/Controllers/ReminderController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Notes.Application.Commands.Create;
+using Notes.Controllers;
+using Notes.Domain.Models;
+
+namespace Notes.WebApi.Controllers
+{
+    public class ReminderController : ControllerGeneric<Reminder, CreateReminderCommand>
+    {
+        public ReminderController(IMediator mediator) : base(mediator)
+        {
+        }
+    }
+}
---
+++ Notes.WebApi/Controllers/TagController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Notes.Application.Commands.Create;
+using Notes.Controllers;
+using Notes.Domain.Models;
+
+namespace Notes.WebApi.Controllers
+{
+    public class TagController : ControllerGeneric<Tag, CreateTagCommand>
+    {
+        public TagController(IMediator mediator) : base(mediator)
+        {
+        }
+    }
+}
